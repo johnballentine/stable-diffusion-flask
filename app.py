645b64f260dcf689327dd5d1bf6ff34143606ba0
John Ballentine <johnballentine10@gmail.com>
2022-09-02 00:11:17 +0000

    Added front-end to txt2img

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template, request
 from scripts.txt2img_flask import *
+from base64 import b64encode
+from urllib.parse import quote
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    img = generate("an astronaut riding on a horse, artstation")
-    return Response(img, mimetype="image/png")
+@app.route('/txt2img')
+def page_txt2img():
+    return render_template("generate.html")
+
+@app.route('/txt2img/endpoint', methods=['POST'])
+def page_txt2img_endpoint():
+
+    input = request.get_json()
+
+    print(request.data)
+
+    print("Generating: "+input["prompt"])
+
+    img = generate(input["prompt"])
+
+    img_data = b64encode(img.getvalue()).decode('ascii')
+    return 'data:image/png;base64,' + quote(img_data)
+
+
+    # Response(img, mimetype="image/png")
