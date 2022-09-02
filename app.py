1a02565ddad4fb08d446b70847c8ac7cc305d3e1
John Ballentine <johnballentine10@gmail.com>
2022-09-02 17:15:13 +0000

    Made Txt2Img into a class. Added param getter function for front-end. TODO: Put frontend params in form

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, Response, render_template, request
-from scripts.txt2img_flask import *
 from base64 import b64encode
 from urllib.parse import quote
 from dotenv import load_dotenv
+from inspect import getfullargspec
+import os
 
 # Local imports
-from models import *
+from models import db, Generation
+from scripts.txt2img_flask import Txt2Img
 
 app = Flask(__name__)
 load_dotenv()
@@ -19,6 +21,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+def get_params(func, exclude_keys=["self"]):
+    spec = getfullargspec(func)
+    padded_defaults = (None,) * (len(spec.args) - len(spec.defaults)) + spec.defaults
+
+    params = dict(zip(spec.args, padded_defaults))
+    for key in exclude_keys:
+        params.pop(key)
+
+    return params
 
 def save_generation(opt, image_data, request_headers="", request_raw=""):
     generation = Generation(
@@ -34,44 +46,30 @@ def save_generation(opt, image_data, request_headers="", request_raw=""):
 
 @app.route('/generate')
 def page_generate():
-    return render_template("generate.html")
+
+    excludes = ['self', 'prompt', 'outdir', 'laion400m',
+                'from_file', 'config', 'ckpt']
+
+    opt = get_params(Txt2Img.__init__, excludes)
+
+    return render_template("generate.html", opt=opt)
 
 @app.route('/txt2img/endpoint', methods=['POST'])
 def page_txt2img_endpoint():
 
     input = request.get_json()
+    """opt = dict(txt2img_opt_defaults)
 
-    opt = {
-        "prompt": input["prompt"],
-        "outdir": "outputs/txt2img-samples",
-        "skip_grid": None,
-        "skip_save": None,
-        "ddim_steps": 50,
-        "plms": True,
-        "laion400m": None,
-        "fixed_code": None,
-        "ddim_eta": 0.0,
-        "n_iter": 1,
-        "H": 512,
-        "W": 512,
-        "C": 4,
-        "f": 8,
-        "n_samples": 1,
-        "n_rows": 0,
-        "scale": 7.5,
-        "from_file": None,
-        "config": "configs/stable-diffusion/v1-inference.yaml",
-        "ckpt": "models/ldm/stable-diffusion-v1/model.ckpt",
-        "seed": 42,
-        "precision": "autocast"
-    }
+    opt["prompt"] = input["prompt"]"""
 
-    print("Generating: "+opt["prompt"])
+    print("Generating: "+input["prompt"])
 
-    image = generate(opt)
+    generation = Txt2Img(input["prompt"])
+
+    image = generation.generate()
     image_data = 'data:image/png;base64,' + quote(b64encode(image.getvalue()).decode('ascii'))
 
-    save_generation(opt=opt,
+    save_generation(opt={"test":"test"},
                     image_data=image_data,
                     request_headers=str(request.headers),
                     request_raw=request.get_data(as_text=True))
--- scripts/txt2img_flask.py
+++ scripts/txt2img_flask.py
@@ -21,224 +21,229 @@ from ldm.models.diffusion.plms import PLMSSampler
 from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
 from transformers import AutoFeatureExtractor
 
-# imports addition
+# imports for flask version
 import io
 
 
-# load safety model
-safety_model_id = "CompVis/stable-diffusion-safety-checker"
-safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
-safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
+class Txt2Img():
+    def __init__(self, prompt="an astronaut riding a horse",
+                 outdir="outputs/txt2img-samples",
+                 skip_grid=None, skip_save=None, ddim_steps=50, plms=True,
+                 laion400m=None, fixed_code=None, ddim_eta=0.0, n_iter=1,
+                 H=512, W=512, C=4, f=8, n_samples=1, n_rows=0, scale=7.5,
+                 from_file=None, config="configs/stable-diffusion/v1-inference.yaml",
+                 ckpt="models/ldm/stable-diffusion-v1/model.ckpt", seed=42,
+                 precision="autocast"):
+
+        self.prompt=prompt
+        self.outdir=outdir
+        self.skip_grid=skip_grid
+        self.skip_save=skip_save
+        self.ddim_steps=ddim_steps
+        self.plms=plms
+        self.laion400m=laion400m
+        self.fixed_code=fixed_code
+        self.ddim_eta=ddim_eta
+        self.n_iter=n_iter
+        self.H=H
+        self.W=W
+        self.C=C
+        self.f=f
+        self.n_samples=n_samples
+        self.n_rows=n_rows
+        self.scale=scale
+        self.from_file=from_file
+        self.config=config
+        self.ckpt=ckpt
+        self.seed=seed
+        self.precision=precision
+
+        # load safety model
+        self.safety_model_id = "CompVis/stable-diffusion-safety-checker"
+        self.safety_feature_extractor = AutoFeatureExtractor.from_pretrained(self.safety_model_id)
+        self.safety_checker = StableDiffusionSafetyChecker.from_pretrained(self.safety_model_id)
+
+    def chunk(self, it, size):
+        it = iter(it)
+        return iter(lambda: tuple(islice(it, size)), ())
 
 
-def chunk(it, size):
-    it = iter(it)
-    return iter(lambda: tuple(islice(it, size)), ())
+    def numpy_to_pil(self, images):
+        """
+        Convert a numpy image or a batch of images to a PIL image.
+        """
+        if images.ndim == 3:
+            images = images[None, ...]
+        images = (images * 255).round().astype("uint8")
+        pil_images = [Image.fromarray(image) for image in images]
+
+        return pil_images
 
 
-def numpy_to_pil(images):
-    """
-    Convert a numpy image or a batch of images to a PIL image.
-    """
-    if images.ndim == 3:
-        images = images[None, ...]
-    images = (images * 255).round().astype("uint8")
-    pil_images = [Image.fromarray(image) for image in images]
+    def load_model_from_config(self, config, ckpt, verbose=False):
+        print(f"Loading model from {ckpt}")
+        pl_sd = torch.load(ckpt, map_location="cpu")
+        if "global_step" in pl_sd:
+            print(f"Global Step: {pl_sd['global_step']}")
+        sd = pl_sd["state_dict"]
+        model = instantiate_from_config(config.model)
+        m, u = model.load_state_dict(sd, strict=False)
+        if len(m) > 0 and verbose:
+            print("missing keys:")
+            print(m)
+        if len(u) > 0 and verbose:
+            print("unexpected keys:")
+            print(u)
 
-    return pil_images
-
-
-def load_model_from_config(config, ckpt, verbose=False):
-    print(f"Loading model from {ckpt}")
-    pl_sd = torch.load(ckpt, map_location="cpu")
-    if "global_step" in pl_sd:
-        print(f"Global Step: {pl_sd['global_step']}")
-    sd = pl_sd["state_dict"]
-    model = instantiate_from_config(config.model)
-    m, u = model.load_state_dict(sd, strict=False)
-    if len(m) > 0 and verbose:
-        print("missing keys:")
-        print(m)
-    if len(u) > 0 and verbose:
-        print("unexpected keys:")
-        print(u)
-
-    model.cuda()
-    model.eval()
-    return model
+        model.cuda()
+        model.eval()
+        return model
 
 
-def put_watermark(img, wm_encoder=None):
-    if wm_encoder is not None:
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        img = wm_encoder.encode(img, 'dwtDct')
-        img = Image.fromarray(img[:, :, ::-1])
-    return img
+    def put_watermark(self, img, wm_encoder=None):
+        if wm_encoder is not None:
+            img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+            img = wm_encoder.encode(img, 'dwtDct')
+            img = Image.fromarray(img[:, :, ::-1])
+        return img
 
 
-def load_replacement(x):
-    try:
-        hwc = x.shape
-        y = Image.open("assets/rick.jpeg").convert("RGB").resize((hwc[1], hwc[0]))
-        y = (np.array(y)/255.0).astype(x.dtype)
-        assert y.shape == x.shape
-        return y
-    except Exception:
-        return x
+    def load_replacement(self, x):
+        try:
+            hwc = x.shape
+            y = Image.open("assets/rick.jpeg").convert("RGB").resize((hwc[1], hwc[0]))
+            y = (np.array(y)/255.0).astype(x.dtype)
+            assert y.shape == x.shape
+            return y
+        except Exception:
+            return x
 
 
-def check_safety(x_image):
-    safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
-    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
-    assert x_checked_image.shape[0] == len(has_nsfw_concept)
-    for i in range(len(has_nsfw_concept)):
-        if has_nsfw_concept[i]:
-            x_checked_image[i] = load_replacement(x_checked_image[i])
-    return x_checked_image, has_nsfw_concept
+    def check_safety(self, x_image):
+        safety_checker_input = self.safety_feature_extractor(self.numpy_to_pil(x_image), return_tensors="pt")
+        x_checked_image, has_nsfw_concept = self.safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
+        assert x_checked_image.shape[0] == len(has_nsfw_concept)
+        for i in range(len(has_nsfw_concept)):
+            if has_nsfw_concept[i]:
+                x_checked_image[i] = self.load_replacement(x_checked_image[i])
+        return x_checked_image, has_nsfw_concept
 
 
-def generate(opt):
-    """opt = {
-        "prompt": "an astronaut riding a horse",
-        "outdir": "outputs/txt2img-samples",
-        "skip_grid": None,
-        "skip_save": None,
-        "ddim_steps": 50,
-        "plms": True,
-        "laion400m": None,
-        "fixed_code": None,
-        "ddim_eta": 0.0,
-        "n_iter": 1,
-        "H": 512,
-        "W": 512,
-        "C": 4,
-        "f": 8,
-        "n_samples": 1,
-        "n_rows": 0,
-        "scale": 7.5,
-        "from_file": None,
-        "config": "configs/stable-diffusion/v1-inference.yaml",
-        "ckpt": "models/ldm/stable-diffusion-v1/model.ckpt",
-        "seed": 42,
-        "precision": "autocast"
-    }"""
+    def generate(self):
+        if self.laion400m:
+            print("Falling back to LAION 400M model...")
+            self.config = "configs/latent-diffusion/txt2img-1p4B-eval.yaml"
+            self.ckpt = "models/ldm/text2img-large/model.ckpt"
+            self.outdir = "outputs/txt2img-samples-laion400m"
 
-    prompt = opt['prompt']
+        seed_everything(self.seed)
 
-    if opt["laion400m"]:
-        print("Falling back to LAION 400M model...")
-        opt["config"] = "configs/latent-diffusion/txt2img-1p4B-eval.yaml"
-        opt["ckpt"] = "models/ldm/text2img-large/model.ckpt"
-        opt["outdir"] = "outputs/txt2img-samples-laion400m"
+        config = OmegaConf.load(f"{self.config}")
+        model = self.load_model_from_config(config, f"{self.ckpt}")
 
-    seed_everything(opt["seed"])
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        model = model.to(device)
 
-    config = OmegaConf.load(f"{opt['config']}")
-    model = load_model_from_config(config, f"{opt['ckpt']}")
+        if self.plms:
+            sampler = PLMSSampler(model)
+        else:
+            sampler = DDIMSampler(model)
 
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model = model.to(device)
+        os.makedirs(self.outdir, exist_ok=True)
+        outpath = self.outdir
 
-    if opt['plms']:
-        sampler = PLMSSampler(model)
-    else:
-        sampler = DDIMSampler(model)
+        print("Creating invisible watermark encoder (see https://github.com/ShieldMnt/invisible-watermark)...")
+        wm = "StableDiffusionV1"
+        wm_encoder = WatermarkEncoder()
+        wm_encoder.set_watermark('bytes', wm.encode('utf-8'))
 
-    os.makedirs(opt['outdir'], exist_ok=True)
-    outpath = opt['outdir']
+        batch_size = self.n_samples
+        n_rows = self.n_rows if self.n_rows > 0 else batch_size
+        if not self.from_file:
+            assert self.prompt is not None
+            data = [batch_size * [self.prompt]]
 
-    print("Creating invisible watermark encoder (see https://github.com/ShieldMnt/invisible-watermark)...")
-    wm = "StableDiffusionV1"
-    wm_encoder = WatermarkEncoder()
-    wm_encoder.set_watermark('bytes', wm.encode('utf-8'))
+        else:
+            print(f"reading prompts from {self.from_file}")
+            with open(self.from_file, "r") as f:
+                data = f.read().splitlines()
+                data = list(self.chunk(data, batch_size))
 
-    batch_size = opt['n_samples']
-    n_rows = opt['n_rows'] if opt['n_rows'] > 0 else batch_size
-    if not opt['from_file']:
-        assert prompt is not None
-        data = [batch_size * [prompt]]
+        sample_path = os.path.join(outpath, "samples")
+        os.makedirs(sample_path, exist_ok=True)
+        base_count = len(os.listdir(sample_path))
+        grid_count = len(os.listdir(outpath)) - 1
 
-    else:
-        print(f"reading prompts from {opt['from_file']}")
-        with open(opt['from_file'], "r") as f:
-            data = f.read().splitlines()
-            data = list(chunk(data, batch_size))
+        start_code = None
+        if self.fixed_code:
+            start_code = torch.randn([self.n_samples, self.C, self.H // self.f, self.W // self.f], device=device)
 
-    sample_path = os.path.join(outpath, "samples")
-    os.makedirs(sample_path, exist_ok=True)
-    base_count = len(os.listdir(sample_path))
-    grid_count = len(os.listdir(outpath)) - 1
+        precision_scope = autocast if self.precision=="autocast" else nullcontext
 
-    start_code = None
-    if opt['fixed_code']:
-        start_code = torch.randn([opt['n_samples'], opt['C'], opt['H'] // opt['f'], opt['W'] // opt['f']], device=device)
+        # Set up memory file
+        imgbuf = io.BytesIO()
 
-    precision_scope = autocast if opt['precision']=="autocast" else nullcontext
+        with torch.no_grad():
+            with precision_scope("cuda"):
+                with model.ema_scope():
+                    tic = time.time()
+                    all_samples = list()
+                    for n in trange(self.n_iter, desc="Sampling"):
+                        for prompts in tqdm(data, desc="data"):
+                            uc = None
+                            if self.scale != 1.0:
+                                uc = model.get_learned_conditioning(batch_size * [""])
+                            if isinstance(prompts, tuple):
+                                prompts = list(prompts)
+                            c = model.get_learned_conditioning(prompts)
+                            shape = [self.C, self.H // self.f, self.W // self.f]
+                            samples_ddim, _ = sampler.sample(S=self.ddim_steps,
+                                                             conditioning=c,
+                                                             batch_size=self.n_samples,
+                                                             shape=shape,
+                                                             verbose=False,
+                                                             unconditional_guidance_scale=self.scale,
+                                                             unconditional_conditioning=uc,
+                                                             eta=self.ddim_eta,
+                                                             x_T=start_code)
 
-    # Set up memory file
-    imgbuf = io.BytesIO()
+                            x_samples_ddim = model.decode_first_stage(samples_ddim)
+                            x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                            x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
 
-    with torch.no_grad():
-        with precision_scope("cuda"):
-            with model.ema_scope():
-                tic = time.time()
-                all_samples = list()
-                for n in trange(opt['n_iter'], desc="Sampling"):
-                    for prompts in tqdm(data, desc="data"):
-                        uc = None
-                        if opt['scale'] != 1.0:
-                            uc = model.get_learned_conditioning(batch_size * [""])
-                        if isinstance(prompts, tuple):
-                            prompts = list(prompts)
-                        c = model.get_learned_conditioning(prompts)
-                        shape = [opt['C'], opt['H'] // opt['f'], opt['W'] // opt['f']]
-                        samples_ddim, _ = sampler.sample(S=opt['ddim_steps'],
-                                                         conditioning=c,
-                                                         batch_size=opt['n_samples'],
-                                                         shape=shape,
-                                                         verbose=False,
-                                                         unconditional_guidance_scale=opt['scale'],
-                                                         unconditional_conditioning=uc,
-                                                         eta=opt['ddim_eta'],
-                                                         x_T=start_code)
+                            x_checked_image, has_nsfw_concept = self.check_safety(x_samples_ddim)
 
-                        x_samples_ddim = model.decode_first_stage(samples_ddim)
-                        x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-                        x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+                            x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
 
-                        x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim)
+                            if not self.skip_save:
+                                for x_sample in x_checked_image_torch:
+                                    x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                                    img = Image.fromarray(x_sample.astype(np.uint8))
+                                    img = self.put_watermark(img, wm_encoder)
+                                    img.save(imgbuf, format='PNG')
+                                    base_count += 1
 
-                        x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
+                            if not self.skip_grid:
+                                all_samples.append(x_checked_image_torch)
 
-                        if not opt['skip_save']:
-                            for x_sample in x_checked_image_torch:
-                                x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-                                img = Image.fromarray(x_sample.astype(np.uint8))
-                                img = put_watermark(img, wm_encoder)
-                                img.save(imgbuf, format='PNG')
-                                base_count += 1
+                    if not self.skip_grid:
+                        # additionally, save as grid
+                        grid = torch.stack(all_samples, 0)
+                        grid = rearrange(grid, 'n b c h w -> (n b) c h w')
+                        grid = make_grid(grid, nrow=n_rows)
 
-                        if not opt['skip_grid']:
-                            all_samples.append(x_checked_image_torch)
+                        # to image
+                        grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
+                        img = Image.fromarray(grid.astype(np.uint8))
+                        img = self.put_watermark(img, wm_encoder)
+                        img.save(imgbuf, format='PNG')
+                        grid_count += 1
 
-                if not opt['skip_grid']:
-                    # additionally, save as grid
-                    grid = torch.stack(all_samples, 0)
-                    grid = rearrange(grid, 'n b c h w -> (n b) c h w')
-                    grid = make_grid(grid, nrow=n_rows)
+                    toc = time.time()
 
-                    # to image
-                    grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-                    img = Image.fromarray(grid.astype(np.uint8))
-                    img = put_watermark(img, wm_encoder)
-                    img.save(imgbuf, format='PNG')
-                    grid_count += 1
+        print(f"Your samples are ready and waiting for you here: \n{outpath} \n"
+              f" \nEnjoy.")
 
-                toc = time.time()
+        imgbuf.seek(0)  # reset buffer position in memory
 
-    print(f"Your samples are ready and waiting for you here: \n{outpath} \n"
-          f" \nEnjoy.")
-
-    imgbuf.seek(0)  # reset buffer position in memory
-
-    return imgbuf
+        return imgbuf
